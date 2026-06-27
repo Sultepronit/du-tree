e13e229db9d46210c14710c19a9683ba61f580c9
Sultepronit <sultepronit@gmail.com>
2026-06-27 23:00:14 +0300

    improved work with symlinks

--- back/explorer/symlinks.go
+++ back/explorer/symlinks.go
@@ -8,21 +8,27 @@ import (
 
 func getRealEntity(path string, name string) (typ string, realPath string) {
 	fullPath := path + "/" + name
-	realPath, err := filepath.EvalSymlinks(fullPath)
+	target, err := os.Readlink(fullPath)
+	if err != nil {
+		return "brk", "?"
+	}
+
+	realPath, err = filepath.EvalSymlinks(fullPath)
 	if err == nil {
 		info, err := os.Stat(realPath)
 		if err != nil {
 			log.Println(err)
 			return "", ""
 		}
-		return info.Mode().Type().String()[0:1], realPath
+		// return info.Mode().Type().String()[0:1], realPath
+		return info.Mode().Type().String()[0:1], target
 	}
-
 	// no such file or directory
-	target, err := os.Readlink(fullPath)
-	if err != nil {
-		return "brk", "?"
-	}
+
+	// target, err := os.Readlink(fullPath)
+	// if err != nil {
+	// 	return "brk", "?"
+	// }
 
 	return "brk", target
 }
