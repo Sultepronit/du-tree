278c44359d52a9e0512f1252185599d3cd3a7ba7
sultepronit <sultepronit@gmail.com>
2026-06-30 14:41:35 +0300

    improved css reload; improved the design

--- front2/src/main-dev.ts
+++ front2/src/main-dev.ts
@@ -6,6 +6,21 @@ updateAccessWidget()
 // /usr/share/doc
 // /var/log
 
+function modUrl(link: HTMLLinkElement) {
+    const url = new URL(link.href)
+    url.searchParams.set("v", Date.now().toString())
+    link.href = url.href
+}
+
+function modAllCss() {
+    const links = document.querySelectorAll(
+        'link[rel="stylesheet"][href*="css"]'
+    ) as NodeListOf<HTMLLinkElement>
+    links.forEach(link => modUrl(link))
+}
+
+modAllCss()
+
 const eventSource = new EventSource("/sse-css")
 eventSource.addEventListener("css-update", event => {
     console.log("Style update:", event.data)
@@ -14,14 +29,10 @@ eventSource.addEventListener("css-update", event => {
     const link = document.querySelector(
         `link[rel="stylesheet"][href*="${name}"]`
     ) as HTMLLinkElement
-    if (link) {
-        const url = new URL(link.href)
-        url.searchParams.set("v", Date.now().toString())
 
-        link.href = url.href
-        // console.log("🎨 CSS оновлено без перезавантаження сторінки!")
-    }
+    if (link) modUrl(link)
 })
+
 eventSource.onerror = err => {
     console.error(err)
 }
