d88ee92a772a9b9db37ddfd84303fc2730131341
sultepronit <sultepronit@gmail.com>
2026-07-26 19:49:44 +0300

    so... path input just killed me... but I promise to kill him!

--- internal/explorer/checkPath.go
+++ internal/explorer/checkPath.go
@@ -3,18 +3,68 @@ package explorer
 import (
 	"du-tree/internal/models"
 	"errors"
+	"fmt"
+	"strings"
 
 	"os"
 	"path/filepath"
 )
 
-func CheckPath(path string) *models.Path {
-	// fmt.Println("path:", path)
-	// filepath.Clean()?
+// if path[0] == '~' {
+// 	if len(path) > 1 && path[1] != '/' {
+// 		return nil
+// 	}
 
+// 	home, err := os.UserHomeDir()
+// 	if err != nil {
+// 		fmt.Println("checkPath:", err)
+// 	} else {
+// 		if path == "~" || path == "~/" {
+// 			path = home + "/"
+// 		}
+// 		path = filepath.Join(home, path[1:])
+// 	}
+// }
+
+func stepBack(path string) string {
+	fmt.Println("path1:", path)
+	// abs, err := filepath.Abs(path)
+	// fmt.Println("abs:", abs, err)
+	prev := filepath.Dir(path)
+
+	fmt.Println(path, prev, filepath.Base(path))
+
+	if prev[0] != path[0] && filepath.Clean(path)[0] != filepath.Clean(prev)[0] {
+		fmt.Println(filepath.Clean(path), filepath.Clean(prev))
+		return ""
+	}
+
+	return prev
+}
+
+func CheckPath(path string) *models.Path {
+	fmt.Println("path0:", path)
 	re := models.Path{
 		Current: "ok",
 		Next:    make([]models.PathDetail, 0, 5),
+	}
+
+	// fmt.Println("path:", path)
+	// filepath.Clean()?
+
+	// if strings.Contains(path, "/") && path[len(path)-1] != '/' {
+	// if strings.LastIndex(path, "/") != len(path)-1 {
+	// 	path = filepath.Dir(path)
+	// }
+
+	if !(strings.HasSuffix(path, "/") || strings.HasSuffix(path, ".")) {
+		if IsAccessible(path, "") {
+			path = stepBack(path)
+			if path == "" {
+				return nil
+			}
+		}
+
 	}
 
 	for i := range 100 {
@@ -42,7 +92,7 @@ func CheckPath(path string) *models.Path {
 					typ, linkTo := GetLinkInfo(path, e.Name())
 					if typ == "d" {
 						next := models.PathDetail{Name: e.Name(), Link: linkTo}
-						// if !IsAccessible(realPath, "") {
+
 						if !IsAccessible(path, e.Name()) {
 							next.IsLocked = true
 						}
@@ -53,13 +103,19 @@ func CheckPath(path string) *models.Path {
 			return &re
 		}
 
-		// path = filepath.Dir(path)
-		prev := filepath.Dir(path)
-		if prev[0] != path[0] {
+		// prev := filepath.Dir(path)
+		// // fmt.Println(path, prev)
+
+		// if prev[0] != path[0] && filepath.Clean(path)[0] != filepath.Clean(prev)[0] {
+		// 	fmt.Println(filepath.Clean(path), filepath.Clean(prev))
+		// 	return nil
+		// }
+
+		// path = prev
+		path = stepBack(path)
+		if path == "" {
 			return nil
 		}
-
-		path = prev
 	}
 
 	return nil
